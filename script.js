3338ba135227259d05ed62acbf7e6578de9f1c6b
Desmond <desmondfon@yahoo.com>
2022-10-24 02:28:14 +0100

    fixed hamburger close icon

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ const menu = document.getElementById('menu');
 hamburg.addEventListener('click', ()=>{
     if(menu.classList.contains('hidden')){
         menu.classList.remove('hidden');
-        hamburg.innerHTML = `<img src="images/icon-close.svg" alt="" class="w-8" >`;
     }else{
         menu.classList.add('hidden');
     }
+
+    if (hamburg.getAttribute("src") == "images/icon-hamburger.svg") {
+              hamburg.src = "images/icon-close.svg";
+            } else {
+              hamburg.src = "images/icon-hamburger.svg";
+            }
 })
